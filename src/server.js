649b31b0c898ec43fff9ba728bf763ea75b780fa
Zhenia Verbitskaya <jvzns17@gmail.com>
2024-12-26 15:10:19 +0200

    add hw7-swagger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+import * as fs from "node:fs";
 import path from "node:path";
 import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
 import contactRouter from "./routers/contacts.js";
 import authRouter from "./routers/auth.js";
 
@@ -10,10 +12,14 @@ import { authenticate } from "./middlewares/authenticate.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 
+const swaggerDocument = JSON.parse(fs.readFileSync(path.resolve("docs/swagger.json"), "utf-8"));
+
 const PORT = process.env.PORT || 3000;
 
 export function setupServer() {
   const app = express();
+
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
   app.use("/photos", express.static(path.resolve("src/public/photos")));
 
